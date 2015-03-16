c395c0189823db66bebec0ce1974bd7315283937
Alexei <diogene.din.pizdiuseni@gmail.com>
2015-03-16 06:54:20 +0200

    Replace react dep with react/addons

--- it/index.js
+++ it/index.js
@@ -1,4 +1,4 @@
-var React = require('react');
+var React = require('react/addons');
 var Translator = require('directive-components/translator');
 var Show = require('directive-components/show').Component;
 var Hide = require('directive-components/hide').Component;
